87e76064751f8581945fba953825d6d8c2030eea
lyy <liangyunyu@gmail.com>
2017-03-23 02:11:16 +0800

    implement is_member_of

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies do
+    member do
+      review :join
+      review :quit
+    end
     resources :reviews
   end
+
+  namespace :account do
+    resources :movies
+    resources :reviews
+  end
+
   root 'movies#index'
-  # root 'welcome#index'
+  #root 'welcome#index'
 end
