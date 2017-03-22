e2c317d233f0825d73658d8ba6e38787e833cde5
lyy <liangyunyu@gmail.com>
2017-03-23 00:21:40 +0800

    ch02-03

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,48 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movies_path, notice: "Update Success!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path, alert: "Movie Deleted!"
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :description)
+  end
+
+end
