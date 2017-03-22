5d68a4ca3a7b68fe11c90c3814466604e6ee1315
lyy <liangyunyu@gmail.com>
2017-03-23 04:12:08 +0800

    check_favorite

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,12 @@
 class ReviewsController < ApplicationController
   before_filter :authenticate_user!, :only => [:new, :create]
+  before_action :check_favorite, only: [:new, :create]
+
   def new
     @movie = Movie.find(params[:movie_id])
     @review = Review.new
   end
+
   def create
     @movie = Movie.find(params[:movie_id])
     @review = Review.new(review_params)
@@ -15,7 +18,17 @@ class ReviewsController < ApplicationController
       render :new
     end
   end
+
   private
+
+  def check_favorite
+    @movie = Movie.find(params[:movie_id])
+    if !current_user.is_member_of?(@movie)
+      flash[:alert] = "你需要先收藏影片再评论！"
+      redirect_to movie_path(@movie)
+    end
+  end
+
   def review_params
     params.require(:review).permit(:content)
   end
